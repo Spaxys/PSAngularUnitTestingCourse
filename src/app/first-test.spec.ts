ab1dbaf605eb66d8fa0d44fa5a213227f9d7e4a7
Daniel Webiörn <spaxys@live.se>
2018-10-14 05:09:57 +0200

    Ch1: WIP adding my first test

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,14 @@
+describe('my first test ', () => {
+    let sut;
+
+    beforeEach(() => {
+        sut = {}
+    })
+    
+    it('should be true if true', () => {
+        //arrange
+        sut.a = false;
+
+        //act
+    })
+})
